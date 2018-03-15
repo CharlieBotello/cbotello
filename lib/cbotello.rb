2921978db5bdf4e3e941d3118d26c04aeaa9dcf4
CharlieBotello <charlieabotello@gmail.com>
2018-03-15 18:21:38 -0500

    first commit

---
+++ lib/cbotello.rb
@@ -0,0 +1,9 @@
+require "cbotello/version"
+
+module Cbotello
+  class BlowDryer
+    def dry
+      'say ooooooooooooooooooooooooooo'
+    end
+  end
+end
---
+++ spec/cbotello_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cbotello do
+  it "has a version number" do
+    expect(Cbotello::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
